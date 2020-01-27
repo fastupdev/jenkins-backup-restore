5c720cdff4d45d48872f965b8d1a09a033dd1b97
Venkata Surya Lolla <surya.lolla@levvel.io>
2020-01-27 18:34:57 -0500

    Included entrypoint and some changes

--- setup.py
+++ setup.py
@@ -1,27 +1,43 @@
 from setuptools import setup, find_packages
+from os import path
 
 with open('requirements.txt') as f:
 	requirements = f.readlines()
 
-long_description = 'Jenkins backup the /var/jenkins_home directory and restore it. \
-Optional flags to store in local directory and push and pull from an s3 bucket.'
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name='jenkins-backup-restore',
-	version='1.0',
+	version='1.0.0',
 	author='Surya Lolla',
 	author_email='suryasaicharan93@gmail.com',
-	description='Jenkins backup and restore python cli tool with arguments.',
+	description='A Jenkins backup and restore python cli tool with arguments.',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url='https://github.com/venkatalolla/jenkins-backup-restore.git',
 	license='MIT',
 	packages=find_packages(),
+	entry_points={
+		'console_scripts': [
+			'jenkins-backup-restore = main.jenkins_backup_restore:main'
+		]
+	},
 	classifiers=(
-		"Programming Language :: Python :: 3",
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Operations and Maintenance",
+		"Topic :: Software Development :: Disaster Recovery Tools",
 		"License :: OSI Approved :: MIT License",
-		'Operating System :: MacOS :: MacOS X',
-		'Operating System :: POSIX',
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: POSIX",
 	),
 	keywords='Jenkins backup and restore',
 	install_requires=requirements,
