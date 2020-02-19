aacf394a62922aee77cc0af73c6d5204aeeb5242
Venkata Surya Lolla <surya.lolla@levvel.io>
2020-02-19 18:31:30 -0500

    Replaced the version according to the tool version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
+import io
+import os
 from os import path
 
 from setuptools import setup, find_packages
-import os
-import io
 
 
 def read(reqpath, encoding='utf-8'):
@@ -17,11 +17,10 @@ def get_install_requirements(reqpath):
         req
         for req in content.split("\n")
         if req != '' and not req.startswith('#')
-    ]
+        ]
 
 
 INSTALL_REQUIRES = get_install_requirements("requirements.txt")
-
 
 here = path.abspath(path.dirname(__file__))
 
@@ -31,7 +30,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='jenkins-backup-restore-cli',
-    version='1.0.7',
+    version='1.1.0',
     author='Surya Lolla',
     author_email='suryasaicharan93@gmail.com',
     description='A Jenkins backup and restore python cli tool with arguments.',
@@ -42,9 +41,9 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'jenkins-backup-restore-cli = main.jenkins_backup_restore_cli:jenkins_backup_restore_cli'
-        ]
-    },
+            'jenkins-backup-restore-cli = jenkins_backup_restore_cli.jenkins_backup_restore_cli:jenkins_backup_restore_cli'
+            ]
+        },
     classifiers=(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: End Users/Desktop",
@@ -60,8 +59,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
-    ),
+        ),
     keywords='Jenkins backup and restore cli',
     install_requires=INSTALL_REQUIRES,
     zip_safe=False
-)
+    )
