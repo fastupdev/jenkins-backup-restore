0990d195494ddbabd559764f6f7e0f99740a4cef
Venkata Surya Lolla <surya.lolla@levvel.io>
2020-02-25 23:48:52 -0500

    Included some new links and refactored the classifiers, version bumped from 1.1.3 to 1.1.4

--- setup.py
+++ setup.py
@@ -25,18 +25,21 @@ INSTALL_REQUIRES = get_install_requirements("requirements.txt")
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='jenkins-backup-restore-cli',
-    version='1.1.3',
-    author='Surya Lolla',
+    version='1.1.4',
+    author='Venkata Surya Lolla',
     author_email='suryasaicharan93@gmail.com',
     description='A Jenkins backup and restore python cli tool with arguments.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/venkatalolla/jenkins-backup-restore.git',
+    project_urls={
+        "Code": "https://github.com/fastupdev/jenkins-backup-restore",
+        "Issue tracker": "https://github.com/fastupdev/jenkins-backup-restore/issues"
+    },
     license='MIT',
     packages=find_packages(),
     entry_points={
@@ -45,20 +48,22 @@ setup(
             ]
         },
     classifiers=(
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Click",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
-        "Topic :: System :: Systems Administration",
-        "Topic :: System :: Recovery Tools",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Recovery Tools",
         ),
     keywords='Jenkins backup and restore cli',
     install_requires=INSTALL_REQUIRES,
